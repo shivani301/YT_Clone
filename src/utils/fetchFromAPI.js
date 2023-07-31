4ac1dc987a7a76363e47a7737fdaa33083bc546a
Shivani Dixit <110478829+shivani301@users.noreply.github.com>
2023-07-31 17:45:54 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,6 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
+    'X-RapidAPI-Key': '8c9bc6870dmsh590d5b45a760c8dp1b552ajsne358bf247ab6',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
